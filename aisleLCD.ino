18c53421ca597adf987d9b9d88775724364ae82a
YJK <114245163+Y-jin126@users.noreply.github.com>
2023-11-19 19:51:29 +0900

    aisleLCD.ino

--- aisleLCD.ino
+++ aisleLCD.ino
@@ -2,7 +2,7 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <state.h>
-#define aisle 8
+#define aisle 8 //I2C 주소 정의
 
 SoftwareSerial mySerial(4,5);
 
@@ -11,7 +11,7 @@ const int D4 = 6; const int D5 = 7;
 const int D6 = 8; const int D7 = 9;
 LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);
  
-byte c; //시리얼통신으로 상태 송신 받는 변수
+char c; //시리얼통신으로 상태 송신 받는 변수
 byte n; //I2C로 현재역 송신 받는 변수
 volatile boolean done = false;
 
@@ -34,7 +34,8 @@ void LCD(byte seatStatus, byte currentStation){ //LCD 출력 함수
   lcd.print("Empty seat");
    
   if(currentStation == ULSAN || currentStation == BEFORE_DAEJEON){
-    if(seatStatus==1){
+    Serial.write("통신");
+    if(seatStatus=='Y'){
       lcd.setCursor(16,1);
       lcd.print(" ");
      
@@ -53,7 +54,7 @@ void loop() {
   if (mySerial.available()) {
     c = mySerial.read(); //시리얼통신으로 송신받음
    }
-  if (done) { //데이터 수신 확인
+  if (done){ //데이터 수신 확인
     LCD(c,n); //LCD 출력
   }
    delay(1000);
