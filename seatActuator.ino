e41ab3719ad781d88ff7fdc323e8fc38b75352c6
Baeyuj <146004842+Baeyuj@users.noreply.github.com>
2023-11-20 11:49:54 +0900

    Update seatActuator.ino

--- seatActuator.ino
+++ seatActuator.ino
@@ -1,13 +1,15 @@
 volatile boolean seatOccupied = 0; //좌석 착석 여부
 
-const int pinLED = 2;
+const int pinLED[3] = { 7, 8, 9 };
 const int pinVibM = 3;
 
 unsigned long l1, l2; //시간 체크
 unsigned long interval = 10000; //진동이 켜지는 시간
 
 void setup() {
-  pinMode(pinLED, OUTPUT);
+  for(int i=0; i<3; i++){
+    pinMode(pinLED[i], OUTPUT); 
+  }
   SPCR |= _BV(SPE);
   SPCR |= _BV(SPIE);
   SPCR &= ~_BV(MSTR);
@@ -16,12 +18,14 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   l2 = millis();
-  if (seatOccupied == true){ //좌석에 누가 착석했을 때
-    digitalWrite(pinLED, HIGH);
-    if(l2 - l1 >= interval) analogWrite(pinLED, 255);
-  } else { //좌석에 누가 착석하지 않았을 때
-    digitalWrite(pinLED, LOW);
-    analogWrite(pinVibM, 0);
+  if (seatOccupied == true){                            //좌석에 누가 착석했을 때
+    for(int i=0; i<3; i++)                              //LED
+      digitalWrite(pinLED[i], HIGH); 
+    if(l2 - l1 >= interval) analogWrite(pinVibM, 255);  //일정 시간 후 진동 켬
+  } else {                                              //좌석에 누가 착석하지 않았을 때
+    for(int i=0; i<3; i++)                              //LED
+      digitalWrite(pinLED[i], LOW); 
+    analogWrite(pinVibM, 0);                            //진동
   }
 }
 
